441fddb78874a3feebc18c173aa95b816811b4b5
Nathan Fronk <ncfronk@gmail.com>
2021-10-13 21:14:35 -0700

    added subs

---
+++ src/main.cc
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+
+
+void run(void);
+
+int main(void)
+{
+    //init();
+
+    run();
+
+    return 0;
+}
+
+void run(void)
+{
+    while (true) {
+
+    }
+}
